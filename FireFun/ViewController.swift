161f80fd5f63c82fa4dc0142f8740577bdbef32e
Juan Manuel Jimenez Sanchez <juanmjimenezs@gmail.com>
2017-02-23 20:40:44 -0500

    Lectura y escritura en una BD Firebase

--- FireFun/ViewController.swift
+++ FireFun/ViewController.swift
@@ -7,19 +7,49 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myTextField: UITextField!
+    @IBOutlet weak var myTableView: UITableView!
+    
+    var ref: FIRDatabaseReference?
+    var handle: FIRDatabaseHandle?
+    
+    var myList: [String] = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        self.ref = FIRDatabase.database().reference()
+        
+        //Aquí llenamos el arreglo con los elementos en BD
+        self.handle = self.ref?.child("list").observe(.childAdded, with: { (snapshot) in
+            if let item = snapshot.value as? String {
+                self.myList.append(item)
+                self.myTableView.reloadData()
+            }
+        })
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func saveBtn(_ sender: UIButton) {
+        if self.myTextField.text != "" {
+            self.ref?.child("list").childByAutoId().setValue(self.myTextField.text)
+            self.myTextField.text = ""
+        }
     }
-
-
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.myList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        cell.textLabel?.text = self.myList[indexPath.row]
+        
+        return cell
+    }
+}
